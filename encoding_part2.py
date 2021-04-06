c5137a0dd226770d06acb0fd6c6b30252c41121a
Post Shalom <jacobglapkin@gmail.com>
2021-04-06 16:57:47 +0100

    attempt 2 working

--- encoding_part2.py
+++ encoding_part2.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+
 
 def encoding2():
-
     #importing csv file
     df = pd.read_csv('data/income_evaluation.csv', sep=r'\s*,\s*',
                                 header=0, encoding='ascii', engine='python')
@@ -22,3 +23,5 @@ def encoding2():
     df_3 = ohe.transform(df_2.fillna('Missing')).toarray()
     new_dataframe = pd.DataFrame(df_3, columns=ohe.get_feature_names())
     return new_dataframe
+
+print(encoding2())
