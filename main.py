a625ad34f3dd3803a2138f94064cc9e45e8f3ea8
sali011 <sali011@gold.ac.uk>
2021-04-07 17:36:02 +0100

    Mixed Jacob and Kyriazis and mine code to get a final df

--- main.py
+++ main.py
@@ -3,84 +3,95 @@ import pandas as pd
 import csv
 from sklearn.preprocessing import OrdinalEncoder
 
-from prepare_data import load_data
+from prepare_data import encoding2
 from model.random_forest import random_forest_classification
 
 def main():
-    df = load_data()
+    # df = load_data()
     
-    # printing columns
-    columns = df.columns
-    for col in columns:
+    # # printing columns
+    # columns = df.columns
+    # for col in columns:
+    #     print(col)
+    
+    # print("Next Test")
+    # print("\n")
+
+    # # printing head of df
+    # df1 = df.head(20)
+
+    # df2 = df1.dropna()
+    # print(df2.head(20))
+
+    # print('\n')
+
+    # # Number of rows
+    # # index = df.index
+    # # number_of_rows = len(index)
+    # # print(number_of_rows)
+
+    # print('encoding part')
+
+    # # only show 'object' columns
+    # obj_df = df2.select_dtypes(include=['object']).copy()
+    # print(obj_df.head(40))
+
+    # print('\n')
+    # print('encoding part')
+    # print('\n')
+
+    # # encoding df 
+    # ord_enc = OrdinalEncoder()
+
+    # obj_df["workclass_code"] = ord_enc.fit_transform(obj_df[["workclass"]])
+    
+    # obj_df["marital-status_code"] = ord_enc.fit_transform(obj_df[["marital-status"]])
+
+    # obj_df["relationship_code"] = ord_enc.fit_transform(obj_df[["relationship"]])
+
+    # obj_df["race_code"] = ord_enc.fit_transform(obj_df[["race"]])
+
+    # obj_df["sex_code"] = ord_enc.fit_transform(obj_df[["sex"]])
+
+    # obj_df["native-country_code"] = ord_enc.fit_transform(obj_df[["native-country"]])
+
+    # obj_df["income_code"] = ord_enc.fit_transform(obj_df[["income"]])
+
+    # obj_df["AgeRange_code"] = ord_enc.fit_transform(obj_df[["AgeRange"]])
+
+    # print(obj_df.head(10))
+
+    # pen_df = obj_df.drop(['workclass', 'marital-status', 'relationship', 'sex', 'native-country', 'income', 'AgeRange'], axis=1)
+
+    # print(pen_df.head(10))
+
+    # df3 = df2.drop(['workclass', 'marital-status', 'relationship', 'sex', 'native-country', 'income', 'AgeRange'], axis=1)
+
+    # print(df3.head())
+
+    # bigdata = pd.merge(pen_df,df3, on='race')
+
+    # print(bigdata.head(30))
+
+    # final_df = bigdata.drop(['race'], axis=1)
+    # print(final_df.head(40))
+
+    # index = bigdata.index
+    # number_of_rows2 = len(index)
+    # print(number_of_rows2)
+
+    # random_forest_classification(final_df)
+    
+    ##################################
+
+    df = encoding2()
+    print(df.head(20))
+    for col in df.columns:
         print(col)
-    
-    print("Next Test")
-    print("\n")
 
-    # printing head of df
-    df1 = df.head(20)
-
-    df2 = df1.dropna()
-    print(df2.head(20))
-
-    print('\n')
-
-    # Number of rows
-    # index = df.index
-    # number_of_rows = len(index)
-    # print(number_of_rows)
-
-    print('encoding part')
-
-    # only show 'object' columns
-    obj_df = df2.select_dtypes(include=['object']).copy()
-    print(obj_df.head(40))
-
-    print('\n')
-    print('encoding part')
-    print('\n')
-
-    # encoding df 
-    ord_enc = OrdinalEncoder()
-
-    obj_df["workclass_code"] = ord_enc.fit_transform(obj_df[["workclass"]])
-    
-    obj_df["marital-status_code"] = ord_enc.fit_transform(obj_df[["marital-status"]])
-
-    obj_df["relationship_code"] = ord_enc.fit_transform(obj_df[["relationship"]])
-
-    obj_df["race_code"] = ord_enc.fit_transform(obj_df[["race"]])
-
-    obj_df["sex_code"] = ord_enc.fit_transform(obj_df[["sex"]])
-
-    obj_df["native-country_code"] = ord_enc.fit_transform(obj_df[["native-country"]])
-
-    obj_df["income_code"] = ord_enc.fit_transform(obj_df[["income"]])
-
-    obj_df["AgeRange_code"] = ord_enc.fit_transform(obj_df[["AgeRange"]])
-
-    print(obj_df.head(10))
-
-    pen_df = obj_df.drop(['workclass', 'marital-status', 'relationship', 'sex', 'native-country', 'income', 'AgeRange'], axis=1)
-
-    print(pen_df.head(10))
-
-    df3 = df2.drop(['workclass', 'marital-status', 'relationship', 'sex', 'native-country', 'income', 'AgeRange'], axis=1)
-
-    print(df3.head())
-
-    bigdata = pd.merge(pen_df,df3, on='race')
-
-    print(bigdata.head(30))
-
-    final_df = bigdata.drop(['race'], axis=1)
-    print(final_df.head(40))
-
-    index = bigdata.index
+    index = df.index
     number_of_rows2 = len(index)
     print(number_of_rows2)
-
-    random_forest_classification(final_df)
 
 
 if __name__ == "__main__":
--- prepare_data.py
+++ prepare_data.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.utils import shuffle
 import numpy as np
 
+from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+
 
 def load_data():
     """
@@ -55,5 +58,48 @@ def divide_dataset():
     return np.split(reduced_dataset,
                     [int(train_portion * len(reduced_dataset)),
                      int((train_portion + test_portion) * len(reduced_dataset))])
+
+
+def encoding2():
+    #importing csv file
+    df = pd.read_csv('data/income_evaluation.csv', sep=r'\s*,\s*',
+                                header=0, encoding='ascii', engine='python')
+    # dropping unused columns
+    df = df.drop(['fnlwgt', 'education', 'occupation', 'capital-gain', 'capital-loss'], axis=1)
+
+    bins = [15, 24, 55]
+    names = ['15-24', '25-54', '55+']
+    d = dict(enumerate(names, 1))
+    df['AgeRange'] = np.vectorize(d.get)(np.digitize(df['age'], bins))
+    df = df.drop(['age'], axis=1)
+
+    # extract the numerical variables for later use
+    education_data = df['education-num']
+    hours_data = df['hours-per-week']
+    age_group_data = df['AgeRange']
+
+    # using only categorical data columns
+    data_cat = df[['workclass', 'marital-status', 'relationship', 'race', 'sex', 'native-country', 'income']]
+    pd.get_dummies(data_cat, dummy_na=True, drop_first=True)
+    df_2 = data_cat
+
+    # one hot encoding these categorical data columns 
+    ohe = OneHotEncoder(categories='auto', drop='first')
+    ohe.fit(df_2.fillna('Missing'))
+    ohe.get_feature_names(['workclass', 'marital-status', 'relationship', 'race', 'sex', 'native-country', 'income'])
+
+    # putting the encoded columns in a new dataframe
+    df_3 = ohe.transform(df_2.fillna('Missing')).toarray()
+    new_dataframe = pd.DataFrame(df_3, columns=ohe.get_feature_names())
+
+    # add the numerical variables back in
+    merged_ed_dataframe = new_dataframe.join(education_data)
+    merged_hours_dataframe = merged_ed_dataframe.join(hours_data)
+    final_dataframe = merged_hours_dataframe .join(age_group_data)
+
+
+    # displaying the datafram on function call
+    return final_dataframe #, merged_hours_dataframe.to_csv(r'/Users/jacoblapkin/Documents/GitHub/FOMLADS Name.csv', index = False)
+                                  # uncomment the above '#' and change the path to where you want the csv file to go if you want one.
 
 
