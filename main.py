fe8ecd9c27802ab927b5401767ffa8acfab27039
Post Shalom <jacobglapkin@gmail.com>
2021-04-08 15:28:16 +0100

    adding function

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 from model.random_forest import random_forest_classifier
+from knn import knn
 from prepare_data import prepare_data
 
 def main():
     df = prepare_data('data/wine.data')
     # print(df.head())
     random_forest_classifier(df)
-    knn(df)
+    #knn(df)
 
 if __name__ == "__main__":
     main()
