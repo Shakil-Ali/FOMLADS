63d43687abdd1fd22a33d3219fef2d4a23f9bc1f
Post Shalom <jacobglapkin@gmail.com>
2021-04-09 16:40:36 +0100

    added density functions to menu system

--- Visuals/data_set_visuals.py
+++ Visuals/data_set_visuals.py
@@ -57,14 +57,67 @@ def box_whisker_plot(name, input_plot):
 # **************************************************
 # THIS IS A FUNCTION THAT SHOWS THE DENSITIES BELOW
 # **************************************************
-def density(plot):
+def density():
     plt.style.use('dark_background')
     df = pd.read_csv('FOMLADS Name.csv')
-    sns.distplot(plot, hist = False, kde = True,
+    print("choose a density function")
+    print("****************************************")
+    print("input [1] to view Alcohol")
+    print("input [2] to view malic_acid")
+    print("input [3] to view ash")
+    print("input [4] to view alcalinity_of_ash")
+    print("input [5] to view magnesium")
+    print("input [6] to view total_phenols")
+    print("input [7] to view flavanoids")
+    print("input [8] to view nonflavanoid_phenols")
+    print("input [9] to view proanthocyanins")
+    print("input [10] to view color_intensity")
+    print("input [11] to view hue")
+    print("input [12] to view od280/od315_of_diluted_wines")
+    print("input [13] to view proline")
+    print("****************************************")
+    choice = int(input("Choice: "))
+    print("****************************************")
+    if choice == 1:
+        sns.distplot(df_alc, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 2:
+        sns.distplot(df_met, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 3:
+        sns.distplot(df_ash, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 4:
+        sns.distplot(df_alcalin, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 5:
+        sns.distplot(df_mag, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 6:
+        sns.distplot(df_phen, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 7:
+        sns.distplot(df_flav, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 8:
+        sns.distplot(df_nonflav, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 9:
+        sns.distplot(df_proan, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 10:
+        sns.distplot(df_color, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 11:
+        sns.distplot(df_hue, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 12:
+        sns.distplot(df_od, hist = False, kde = True,
+                 kde_kws = {'shade': True, 'linewidth': 3})
+    elif choice == 13:
+        sns.distplot(df_prol, hist = False, kde = True,
                  kde_kws = {'shade': True, 'linewidth': 3})
     return plt.show()
 
-# uncomment the below line and fill in with argument from the list (line 15-27) ; there is an example already filled in
-#print(density(df_flav))
 
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from model.random_forest import random_forest_classifier
+from Visuals.data_set_visuals import density
 from model.knn import knn
 from prepare_data import prepare_data
 
@@ -15,15 +16,28 @@ def main():
         print("input [1] to view Random Forest Classifier")
         print("input [2] to view K Nearest neighbors")
         print("input [3] to view data set")
+        print("input [4] to view data set statistics and graphs")
         print("****************************************")
 
-        choice = int(input("Make a selection: "))
+        choice = int(input("Selection: "))
         if choice == 1:
             random_forest_classifier(df)
         elif choice == 2:
             knn(df)
         elif choice == 3:
             print(df)
+        elif choice == 4:
+            while True:
+                print("****************************************")
+                print("Choose which graph/stat to view")
+                print("****************************************")
+                print("input [1] to view Density Functions")
+                print("input [2] to view K Nearest neighbors")
+                print("input [3] to view data set")
+                print("****************************************")
+                stat_choice = int(input("Choice: "))
+                if stat_choice == 1:
+                    density()
         else: 
             print(str(choice) + " is not a valid option")
 
