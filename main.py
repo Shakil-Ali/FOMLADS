efc24f3cae357a04d79c5de891a3e6e06d9e033a
Post Shalom <jacobglapkin@gmail.com>
2021-04-09 15:56:27 +0100

    added menu system to main.py
    When you run main.py, there will be a menu system from the command line that allows you to choose which model to view.

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ def main():
     df = prepare_data('data/wine.data')
     # normalized_df=(df-df.mean())/df.std()
     # print(df.head())
-    random_forest_classifier(df)
-    knn(df)
+    print("****************************************")
+    print("Please select an option to view model")
+    print("****************************************")
+    print("input [1] to view Random Forest Classifier")
+    print("input [2] to view K Nearest neighbors")
+    print("input [3] to view data set")
+    print("****************************************")
+
+    choice = int(input("Make a selection: "))
+    if choice == 1:
+        random_forest_classifier(df)
+    elif choice == 2:
+        knn(df)
+    elif choice == 3:
+        print(df)
 
 if __name__ == "__main__":
     main()
