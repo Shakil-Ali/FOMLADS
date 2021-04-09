bbf9f07c253778042be64811641aeafcc665ab6e
Post Shalom <jacobglapkin@gmail.com>
2021-04-09 16:02:57 +0100

    while loop

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ def main():
     df = prepare_data('data/wine.data')
     # normalized_df=(df-df.mean())/df.std()
     # print(df.head())
-    print("****************************************")
-    print("Please select an option to view model")
-    print("****************************************")
-    print("input [1] to view Random Forest Classifier")
-    print("input [2] to view K Nearest neighbors")
-    print("input [3] to view data set")
-    print("****************************************")
+    while True:
+        print("****************************************")
+        print("Please select an option to view model")
+        print("****************************************")
+        print("input [1] to view Random Forest Classifier")
+        print("input [2] to view K Nearest neighbors")
+        print("input [3] to view data set")
+        print("****************************************")
 
-    choice = int(input("Make a selection: "))
-    if choice == 1:
-        random_forest_classifier(df)
-    elif choice == 2:
-        knn(df)
-    elif choice == 3:
-        print(df)
-    else: 
-        print("not a valid option")
+        choice = int(input("Make a selection: "))
+        if choice == 1:
+            random_forest_classifier(df)
+        elif choice == 2:
+            knn(df)
+        elif choice == 3:
+            print(df)
+        else: 
+            print(str(choice) + " is not a valid option")
 
 if __name__ == "__main__":
     main()
