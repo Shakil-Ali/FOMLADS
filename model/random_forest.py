1ac21ffbb5bdb4f6af4a30484b30ad623771fadb
sali011 <sali011@gold.ac.uk>
2021-04-11 16:47:54 +0100

    Random Forest Accuracy - used jacobs accuracy (non scikit)

--- model/random_forest.py
+++ model/random_forest.py
@@ -1,7 +1,10 @@
+import numpy as np
 import matplotlib.pyplot as plt
+
 from plot.plotting_functions import plot_confusion_matrix
 from prepare_data import prepare_data
-from sklearn import metrics
+
+from sklearn.metrics import accuracy_score, f1_score, precision_score
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 
@@ -15,9 +18,19 @@ def random_forest_classifier(file):
     y_pred = clf.predict(X_test)
     plot_confusion_matrix(y_test, y_pred, 'Random Forest')
 
-    # Accuracy
-    print('Random Forest Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+    # Metrics
+    # print('Random Forest Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy_NoSkLearn = np.sum(np.equal(y_test,y_pred))/len(y_test)
+    f1 = f1_score(y_test, y_pred, average="macro")
+    prec_score = precision_score(y_test, y_pred, average="macro")
 
+    print("Random Forest Accuracy with Sklearn: ", accuracy)
+    print("Random Forest Accuracy without Sklearn: ", accuracy_NoSkLearn)
+    print("Random Forest f1 score: ", f1)
+    print("Random Forest Precision Score: ", prec_score)
+
+    # Drawing Decision Tree
     #plt.figure(figsize=(5, 5))
     #     for i in range(len(clf.estimators_)):
     #             tree.plot_tree(clf.estimators_[i])
