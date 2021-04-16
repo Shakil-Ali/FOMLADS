831f619650db6fb0dda5d13fe4bc070276ccb4f7
Post Shalom <jacobglapkin@gmail.com>
2021-04-16 17:07:07 +0100

    created you file

---
+++ model/log_regression.py
@@ -0,0 +1,10 @@
+import math
+
+import pandas as pd
+from evaluation.evaluation import print_evaluation_scores, calculate_cm
+from plot.knn_visuals import k_value_graph
+from plot.plotting_functions import plot_confusion_matrix
+from prepare_data import prepare_data
+
+def log_reg(file):
+    #code goes here
